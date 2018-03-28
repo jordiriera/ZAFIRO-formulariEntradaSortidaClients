c819bfc87d7a9740e44771c1464425bf63194fd0
Jordi Riera Rayo <jriera@zafirohotels.com>
2018-03-28 12:31:56 +0200

    Afegida funcionalitat de insertar dins la base de dades a través del mètode 'send()'

--- controllers/provesController.php
+++ controllers/provesController.php
@@ -51,5 +51,22 @@ class provesController
     // echo '<br>';
     // print_r($important);
   }
+  public function send(){
+
+
+    $comentari = $_POST['comentari'];
+
+    $comentari = !empty($comentari) ? "$comentari" : "NULL";
+
+    //  ***** TESTS *****
+    //print_r('<br>' . 'CONTROLLER  ' . $_POST['telephone']);
+    //print_r('<br>' . 'CONTROLLER ' . $_POST['horaArribada'] . $_POST['horaSortida'] . $comentari);
+
+    userModel::update($_POST['idUser'], $_POST['telephone']);
+    buyModel::update($_POST['idCompra'], $_POST['horaArribada'], $_POST['horaSortida'], $comentari);
+
+    header("Location: ask.php?c=upgrade");
+
+  }
 
 }
